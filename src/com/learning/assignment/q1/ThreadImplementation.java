17dffe41d372a6c8ccbdbceb127daa9457509092
Sharique Ahmed <ahmed.sharique46@gmail.com>
2023-08-28 23:57:45 +0530

    Added Java Code Files

--- src/com/learning/assignment/q1/ThreadImplementation.java
+++ src/com/learning/assignment/q1/ThreadImplementation.java
@@ -1,42 +1,13 @@
 package com.learning.assignment.q1;
 
-import java.io.*;
 import java.util.*;
 
-public class Driver {
+public class ThreadImplementation {
+
 	Map<Project, List<Employee>> projectMap1 = new HashMap<>();
 	List<Employee> elist1 = new ArrayList<>();
 	List<Employee> elist2 = new ArrayList<>();
 	List<Employee> elist3 = new ArrayList<>();
-
-	public void serializeProjectDetails(Map<Project, List<Employee>> projectMap) {
-		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("project_data.ser"))) {
-			oos.writeObject(projectMap);
-			System.out.println("Project details serialized successfully.");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void deserializeAndDisplay() {
-		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("project_data.ser"))) {
-			Map<Project, List<Employee>> deserializedMap = (Map<Project, List<Employee>>) ois.readObject();
-			System.out.println("DeSerialized Data:");
-			for (Map.Entry<Project, List<Employee>> entry : deserializedMap.entrySet()) {
-				Project project = entry.getKey();
-				List<Employee> employees = entry.getValue();
-				System.out.println("The Project");
-				System.out.println(project);
-				System.out.println("Has the following Employees");
-				System.out.println("Employees .............");
-				for (Employee employee : employees) {
-					System.out.println(employee);
-				}
-			}
-		} catch (IOException | ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
 
 	public static void main(String[] args) {
 		Project project1 = new Project("P1", "Music Synthesizer", 23);
@@ -55,7 +26,7 @@ public class Driver {
 		Employee e8 = new Employee("E008", "Suraj", "9383992133", "Malleswaram", 3000);
 		Employee e9 = new Employee("E009", "Manu", "9345193933", "RTNagar", 2000);
 
-		Driver projectSerializer = new Driver();
+		ThreadImplementation projectSerializer = new ThreadImplementation();
 
 		projectSerializer.elist1.add(e1);
 		projectSerializer.elist1.add(e2);
@@ -73,7 +44,10 @@ public class Driver {
 		projectSerializer.projectMap1.put(project2, projectSerializer.elist2);
 		projectSerializer.projectMap1.put(project3, projectSerializer.elist3);
 
-		projectSerializer.serializeProjectDetails(projectSerializer.projectMap1);
-		projectSerializer.deserializeAndDisplay();
+		ProjectSerializer serializer = new ProjectSerializer();
+		serializer.serializeProjectDetails(projectSerializer.projectMap1);
+
+		ProjectDeserializer deserializer = new ProjectDeserializer();
+		deserializer.deserializeAndDisplay();
 	}
 }
